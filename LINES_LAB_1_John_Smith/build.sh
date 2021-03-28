f0f06dc003a1feb532f70ec3e324101a6d5d3bf4
Lucas Lisznianski <lucas@aggati.com>
2021-03-28 13:05:13 +0000

    redesign

---
+++ LINES_LAB_1_John_Smith/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+BR_NAME=buildroot-2021.02
+BR_FILE=${BR_NAME}.tar.bz2
+BR_DL=../${BR_FILE}
+set -e
+if [ ! -f ${BR_DL} ] || ! ( bzip2 -q -t ${BR_DL}); then
+  (  
+     cd ..
+     rm -f ${BR_FILE}
+     wget https://buildroot.org/downloads/${BR_FILE}
+  )
+fi
+tar -xjf ${BR_DL}
+cp BR-config ${BR_NAME}/.config
+cd ${BR_NAME}
+for i in ../patches/*.patch ; do
+  if [ -f $i ]; then
+     patch -p1 < $i
+  fi
+done
+make
+
+
